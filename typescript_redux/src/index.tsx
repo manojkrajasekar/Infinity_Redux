5348f2f69e94d4974bae0e16704adf5aa3791982
manojkrajasekar <manoj.k.rajasekar@gmail.com>
2018-03-24 22:50:48 -0400

    Redux code with TypeScript

--- typescript_redux/src/index.tsx
+++ typescript_redux/src/index.tsx
@@ -1,15 +1,3 @@
-// import * as React from 'react';
-// import * as ReactDOM from 'react-dom';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-// import './index.css';
-
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root') as HTMLElement
-// );
-// registerServiceWorker();
-
 import { createStore } from 'redux';
 import { AddData } from './actions/Data';
 import { Reducer } from './reducers/Reducer';
@@ -17,20 +5,16 @@ import { Reducer } from './reducers/Reducer';
 
 const store = createStore(Reducer);
 
-
 // const price = [6, 2, 4, 5];
-
 const priceData = [9, 8, 7, 88];
 priceData.map((price) => {(
-    store.dispatch((AddData(priceData)))
-  );
-}
+      store.dispatch((AddData(priceData)))
+    );
+  }
 );
 
 // store.dispatch((AddData(price)));
-
- // console.log(values.price.sort());
-
+// console.log(values.price.sort());
 console.log(store.getState());
 
 
